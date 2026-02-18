2a26e6d59e7dec5274420bfb5e8c522fab0f1f94
man-navlakha <mannavlakha1021@gmail.com>
2026-02-18 16:43:50 +0530

    feat: initialize root layout with global styles, metadata, and Microsoft Clarity tracking.

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -62,6 +63,15 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "vj9qj3k6dd");
+          `}
+        </Script>
       </body>
     </html>
   );
